6ed99830221b1637871fc669541b6c17a5454ea1
Jacob Stauch <jstauch@drury.edu>
2019-10-31 17:03:55 -0500

    Added missing word in AST documentation, built JAR, and created script to use it. Will update README shortly.

--- src/bashShell/ast/AST.java
+++ src/bashShell/ast/AST.java
@@ -6,8 +6,10 @@ package bashShell.ast;
 public abstract class AST {
     /**
      *
-     * @param indentLevel
-     * @return
+     * @param indentLevel Dictates the pretty-printing of the AST.
+     *                    Works by repeating a tab character the number
+     *                    of times provided by indentLevel
+     * @return The string representing the AST.
      */
     public abstract String visit(int indentLevel);
 }
